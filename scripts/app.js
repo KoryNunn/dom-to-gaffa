a44da2154b5dde3a363dec655a67fb9c11838101
Kory Nunn <knunn187@gmail.com>
2013-10-28 11:18:49 +1000

    demo cleanup

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,9 @@
 var Gaffa = require('gaffa'),
-    myApp = {},
     gaffa = window.gaffa = new Gaffa(),
     views = gaffa.views.constructors = require('./views'),
     actions = gaffa.actions.constructors = require('./actions'),
     behaviours = gaffa.behaviours.constructors = require('./behaviours'),
     domToGaffa = require('../dom-to-gaffa');
-
-myApp.gaffa = gaffa;
-
 
 window.onload = function(){
     gaffa.load(domToGaffa(gaffa, document.body));
